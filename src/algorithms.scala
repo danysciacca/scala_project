dbc123e8f8d65327bcbfd3e9d3e1460dbe3f52e5
daniele sciacca <daniele18051991@gmail.com>
2017-03-08 14:23:38 +0100

    first commit update

--- src/algorithms.scala
+++ src/algorithms.scala
@@ -2,5 +2,16 @@
   * Created by Daniele on 08/03/2017.
   */
 class algorithms {
+  //regex to nfa(concatenazione, unione, stella di kleene)
+  //in tabella
+
+
+
+
+
+
+
+  //nfa to dfa
+  //in tabella
 
 }
