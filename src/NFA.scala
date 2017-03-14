2e2110d61f999b9da6a11442e56b4c7dd2784dc1
daniele sciacca <daniele18051991@gmail.com>
2017-03-14 13:14:19 +0100

    concat function

--- src/NFA.scala
+++ src/NFA.scala
@@ -2,26 +2,74 @@
   * Created by Daniele on 08/03/2017.
   */
 case class NFA[Q, Stati](trans: (Q, Stati)=> Set[Q], S0:Q, f:Q=>Boolean) {
-  def Union[_Q](another: NFA[_Q, Stati])={
-  def asNFA = this
+  def Union[_Q](another: NFA[_Q, Stati])= {
+    def asNFA = this
 
-  sealed abstract trait dummyS
-  object dummyS extends dummyS  //uno stato iniziale dello stato iniziale.
+    sealed abstract trait dummyS
+    object dummyS extends dummyS //uno stato iniziale dello stato iniziale.
 
-  sealed trait epsi
-  object epsi extends epsi
+    sealed trait epsi
+    object epsi extends epsi
 
-  val _trans:(Either[dummyS, Either[Q, _Q]], Either[Stati, epsi]) => Set[Either[dummyS, Either[Q, _Q]]]={
-    case (Left(dummyS), Right(epsi)) => Set(Right(Left(this.q0)), Right(Right(another.q0)))
-    case (Right(Left(q1)),Left(_x)) => this.trans(q1,_x) map (q => Right(Left(q)))
-    case (Right(Right(q2)),Left(_x)) => another.trans(q2,_x) map (q => Right(Right(q)))
-    case _ => Set.empty
+    val _trans: (Either[dummyS, Either[Q, _Q]], Either[Stati, epsi]) => Set[Either[dummyS, Either[Q, _Q]]] = {
+      case (Left(dummyS), Right(epsi)) => Set(Right(Left(this.q0)), Right(Right(another.q0)))
+      case (Right(Left(q1)), Left(_x)) => this.trans(q1, _x) map (q => Right(Left(q)))
+      case (Right(Right(q2)), Left(_x)) => another.trans(q2, _x) map (q => Right(Right(q)))
+      case _ => Set.empty
+    }
+
+    val _f = (q: Either[dummyS, Either[Q, _Q]]) => q match {
+      //costruisco qui l' NFA
+      case Right(Left(q1)) => this.f(q1) //al solito, parto dal dummy per settare il primo stato (ME STESSO)
+      case Right(Right(q2)) => another.f(q2) // qui il doppio passo a destra per andare avanti
+      case _ => false // in tutti gli altri casi pongi pongi
+    }
+
+
   }
 
-  val _f = (q:Either[dummyS,Either[Q, _Q]]) => q match{  //costruisco qui l' NFA
-      case Right(Left(q1)) => this.f(q1)    //al solito, parto dal dummy per settare il primo stato (ME STESSO)
-      case Right(Right(q2)) => another.f(q2) // qui il doppio passo a destra per andare avanti
-      case _ => false  // in tutti gli altri casi pongi pongi
+
+    //concatenazione
+    def concat[_Q](another:NFA[_Q, Stati])={
+      sealed abstract trait a1
+      object a1 extends a1 //momentaneamente l'ho definito qua, è uno stato generico
+
+      sealed abstract trait q1
+      object q1 extends q1
+
+      sealed abstract trait x
+      object x extends x
+
+      sealed trait epsi
+      object epsi extends epsi
+
+
+      val _trans:(Either[Q, _Q],Either[Stati, epsi])=> Set[Either[Q, _Q]]={
+        case (Left(a1), Right(ε)) if this.f(a1) => Set(Right(another.S0))
+        case (Left(q1), Left(_x))=> this.trans(q1, _x) map (q=>Left(q))
+        case (Right(q2), Left(_x))=> another.trans(q2, _x) map(q=> Right(q))
+        case _=>Set.empty
+
+      }
+      val _f: Either[Q, _Q] => Boolean={//riconosce uno stato
+        case Right(q2)=> another.f(q2)
+        case _=>false
+      }
+      //parte NFA epsi
+
+
+
     }
+
+
+
+
+
+
+
+
+
+
+
 
 }
