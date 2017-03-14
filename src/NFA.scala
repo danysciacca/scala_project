942494c90b31a5fa874fdab193f59cabb5e398a8
Luca Tosto <lucatosto1391@gmail.com>
2017-03-14 11:41:25 +0100

    Union algorithm — no test

--- src/NFA.scala
+++ src/NFA.scala
@@ -13,6 +13,15 @@ case class NFA[Q, Stati](trans: (Q, Stati)=> Set[Q], S0:Q, f:Q=>Boolean) {
 
   val _trans:(Either[dummyS, Either[Q, _Q]], Either[Stati, epsi]) => Set[Either[dummyS, Either[Q, _Q]]]={
     case (Left(dummyS), Right(epsi)) => Set(Right(Left(this.q0)), Right(Right(another.q0)))
+    case (Right(Left(q1)),Left(_x)) => this.trans(q1,_x) map (q => Right(Left(q)))
+    case (Right(Right(q2)),Left(_x)) => another.trans(q2,_x) map (q => Right(Right(q)))
+    case _ => Set.empty
   }
 
+  val _f = (q:Either[dummyS,Either[Q, _Q]]) => q match{
+      case Right(Left(q1)) => this.f(q1)
+      case Right(Right(q2)) => another.f(q2)
+      case _ => false
+    }
+    
 }
