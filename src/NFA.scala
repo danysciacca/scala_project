63bd7d78e9c98a1547956129c886273de185be0d
daniele sciacca <daniele18051991@gmail.com>
2017-03-15 12:58:37 +0100

    classe NFA con epsilon-mosse. Completato

--- src/NFA.scala
+++ src/NFA.scala
@@ -2,13 +2,18 @@
   * Created by Daniele on 08/03/2017.
   */
 case class NFA[Q, Stati](trans: (Q, Stati)=> Set[Q], S0:Q, f:Q=>Boolean) {
+
+
+  sealed trait epsi
+
+
   def Union[_Q](another: NFA[_Q, Stati])= {
     def asNFA = this
 
     sealed abstract trait dummyS
     object dummyS extends dummyS //uno stato iniziale dello stato iniziale.
 
-    sealed trait epsi
+
     object epsi extends epsi
 
     val _trans: (Either[dummyS, Either[Q, _Q]], Either[Stati, epsi]) => Set[Either[dummyS, Either[Q, _Q]]] = {
@@ -24,7 +29,7 @@ case class NFA[Q, Stati](trans: (Q, Stati)=> Set[Q], S0:Q, f:Q=>Boolean) {
       case Right(Right(q2)) => another.f(q2) // qui il doppio passo a destra per andare avanti
       case _ => false // in tutti gli altri casi pongi pongi
     }
-
+    epsiNFA(_trans, Left(dummyS).asInstanceOf[Either[dummyS, Either[Q, _Q]]],_f)
 
   }
 
@@ -33,7 +38,7 @@ case class NFA[Q, Stati](trans: (Q, Stati)=> Set[Q], S0:Q, f:Q=>Boolean) {
     def concat[_Q](another:NFA[_Q, Stati])={
 
 
-      sealed trait epsi
+
       object epsi extends epsi
 
 
@@ -48,11 +53,51 @@ case class NFA[Q, Stati](trans: (Q, Stati)=> Set[Q], S0:Q, f:Q=>Boolean) {
         case Right(q2)=> another.f(q2)
         case _=>false
       }
-      //parte NFA epsi
-
-
+      epsiNFA(_trans, Left(this.S0), _f)
 
     }
+  //stella di kleene
+  def kleene = {
+
+
+    object epsi extends epsi
+
+    val _trans: (Q,Either[Stati,epsi])=>Set[Q] ={
+      case (q,Right(epsi)) if this.f(q) => Set(this.S0)
+      case (q,Right(epsi)) if !this.f(q) => Set.empty
+      case (q,Left(x)) => trans(q,x)
+      case _ => Set.empty
+    }
+    val _f = (q:Q) => q == S0 || f(q)
+
+    epsiNFA(_trans, S0, _f)
+  }
+
+
+
+
+  def epsiNFA[Q, Stati](trans:(Q, Either[Stati, epsi])=> Set[Q], S0:Q, f:Q=> Boolean){
+
+    def chiusura(q: Q): Set[Q] = {
+      object epsi extends epsi
+    var visitato= Set.empty[Q]
+      val queue= new scala.collection.mutable.Queue[Q]()
+
+      queue.enqueue(q)
+      while(!queue.isEmpty){
+        val nuovo_stato=trans(queue.dequeue(), Right(epsi)) -- visitato
+        queue ++= nuovo_stato
+        visitato ++= nuovo_stato
+      }
+      visitato
+    }
+
+  }
+
+
+
+
+
 
 
 
